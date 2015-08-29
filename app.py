b294d3dde3194c43228367da3c51baab1734bedf
geekpradd <pradd@outlook.com>
2015-08-29 14:22:59 +0530

    Added proper logging

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, jsonify
-
+import logging
 from wordnik import *
 apiUrl = 'http://api.wordnik.com/v4'
 apiKey = '294b946d501d05d5620010e788d0bc608443f689077814987'
 client = swagger.ApiClient(apiKey, apiUrl)
 app = Flask(__name__)
+app.debug = True 
+app.logger.setLevel(logging.DEBUG)
+del app.logger.handlers[:]
+
+handler = logging.StreamHandler(stream=sys.stdout)
+handler.setLevel(logging.DEBUG)
+handler.formatter = logging.Formatter(
+    fmt=u"%(asctime)s level=%(levelname)s %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%SZ",
+)
+app.logger.addHandler(handler)
 
 @app.route('/word-of-the-day/')
 def word():
@@ -18,4 +29,4 @@ def word():
 
 	return jsonify(dic)
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
